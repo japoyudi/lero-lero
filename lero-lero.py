bd1427854cf6ea61c06dc77664f584c0ae44f1f8
Daniel Yudi <daniel.ashikaga@usp.br>
2025-03-21 10:13:12 -0300

    Cria esqueleto da versão multi-línguas

--- lero-lero.py
+++ lero-lero.py
@@ -1,12 +1,19 @@
 #!usr/bin/python3
 
 """Gerador de lero-lero.
-Gera frases de efeito sem significado real"""
+Gera frases de efeito sem significado real."""
 
 import random
 
 parte1 = []
 parte2 = []
 parte3 = []
+
+lingua = int(input("Escolha a língua: 1 - português; 2 - inglês\n "))
+
+if lingua == 2;
+	parte1 = []
+	parte2 = []
+	parte3 = [] 
 
 print(random.choice(part1), random.choice(part2), random.choice(part3))
