ec968fe52ea8fb70a4855717c2bf24c3f342d279
gabriel <69213912+gabrielGAGRA@users.noreply.github.com>
2025-03-21 10:18:11 -0300

    Insere os trechos das frases

--- lero-lero.py
+++ lero-lero.py
@@ -5,8 +5,14 @@ Gera frases de efeito sem significado real"""
 
 import random
 
-parte1 = []
-parte2 = []
-parte3 = []
+parte1 = ["O sistema em desenvolvimento",
+"O novo protocolod e comunicacao",
+"O algorimo orimizado"]
+parte2 = ["possui excelente desempenho",
+"oferece garantias de precisao acima da media"
+"preenche uma lacuna significativa"]
+parte3 = ["nas aplicacoes a que se destina",
+"em relacao as opcoes disponiveis do mercado",
+", provendo ampla vantagem competitiva a seus usuarios"]
 
 print(random.choice(part1), random.choice(part2), random.choice(part3))
