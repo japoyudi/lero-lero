059c54748242286168900fa93d4ed1d60c2c4f02
gabriel <69213912+gabrielGAGRA@users.noreply.github.com>
2025-03-21 09:57:58 -0300

    Implementa esqueleto do programa

--- lero-lero.py
+++ lero-lero.py
@@ -2,3 +2,11 @@
 
 """Gerador de lero-lero.
 Gera frases de efeito sem significado real"""
+
+import random
+
+parte1 = []
+parte2 = []
+parte3 = []
+
+print(random.choice(part1), random.choice(part2), random.choice(part3))
